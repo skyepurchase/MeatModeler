f79f002d57375ade6dae5918a0fb1b036f9cccfc
Skye <aidantpurchase@gmail.com>
2021-06-28 14:38:53 +0100

    Added capabilities to remove blurry frames using Laplacian filter

--- imageProcessor.py
+++ imageProcessor.py
@@ -22,12 +22,31 @@ class ImageProcessor:
         ret, frame = cam.read()
         while ret:
             filename = self.path + "Frame" + str(current_frame) + ".jpg"
-            print(filename)
             cv2.imwrite(filename, frame)
             current_frame += 1
             ret, frame = cam.read()
 
         return self.path
+
+    def removeBlur(self):
+        for img in os.listdir(self.path):
+            pathname = os.path.join(self.path, img)
+            src = cv2.imread(pathname, cv2.IMREAD_COLOR)
+
+            if src is None:
+                print("Error opening image!")
+                print(img)
+                break
+
+            src = cv2.GaussianBlur(src, (3, 3), 0)  # Remove noise by blurring image
+
+            src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+            dst = cv2.Laplacian(src_gray, cv2.CV_64F)
+            value = dst.var()
+
+            if value < 5:
+                os.remove(pathname)
 
     def getPath(self):
         return self.path
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     video_filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Input\\WIN_20210628_13_21_13_Pro.mp4"
     processor = imageProcessor.ImageProcessor("C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\")
     processor.split(video_filename)
+    processor.removeBlur()
 
 
 if __name__ == '__main__':
