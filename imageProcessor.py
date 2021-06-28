8d4ca6d06255a166ab5b6bc3cf95ed7082fc9b97
Skye <aidantpurchase@gmail.com>
2021-06-28 15:35:03 +0100

    Joined removeBlur and split adding capabilities to recreate the video without blurry frames

--- imageProcessor.py
+++ imageProcessor.py
@@ -8,45 +8,62 @@ class ImageProcessor:
 
     def split(self, video_path):
         """
-        Splits a video into individual frames as .jpg images.
+        Splits a video into individual frames as .jpg images. All 'blurry' frames are removed during this process
 
         :param video_path: Fully qualified path to the video to be split.
         :return: Fully qualified path to the file containing the split frames.
         """
-        for file in os.listdir(self.path):
-            os.remove(os.path.join(self.path, file))  # Remove the current images
+        # for file in os.listdir(self.path):
+        #     os.remove(os.path.join(self.path, file))  # Remove the current images
 
         cam = cv2.VideoCapture(video_path)
         current_frame = 0
 
         ret, frame = cam.read()
         while ret:
-            filename = self.path + "Frame" + str(current_frame) + ".jpg"
-            cv2.imwrite(filename, frame)
-            current_frame += 1
+            src = cv2.GaussianBlur(frame, (3, 3), 0)  # Remove noise by blurring image slightly
+            src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+            value = cv2.Laplacian(src_gray, cv2.CV_64F).var()  # Apply Laplacian filter to get edges
+
+            if value > 5:  # Suggests the edges are very weak and thus blurry
+                filename = self.path + "Frame" + str(current_frame) + ".jpg"
+                cv2.imwrite(filename, frame)
+                current_frame += 1
+
             ret, frame = cam.read()
 
         return self.path
 
-    def removeBlur(self):
+    def recreateVideo(self):
+        """
+        Reconstructs the video from the non-blurred images
+
+        :return: The input folder contains the non-blurred video
+        """
+        filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Input\\UnblurredVideo.avi"
+
+        img_array = []
+        size = (500, 500)
+
         for img in os.listdir(self.path):
-            pathname = os.path.join(self.path, img)
-            src = cv2.imread(pathname, cv2.IMREAD_COLOR)
+            src = cv2.imread(os.path.join(self.path, img))
 
             if src is None:
-                print("Error opening image!")
+                print("Error loading image!")
                 print(img)
                 break
 
-            src = cv2.GaussianBlur(src, (3, 3), 0)  # Remove noise by blurring image
+            height, width, layers = src.shape
+            size = (width, height)
+            img_array.append(src)
 
-            src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
 
-            dst = cv2.Laplacian(src_gray, cv2.CV_64F)
-            value = dst.var()
+        img_array.reverse()
+        for img in img_array:
+            out.write(img)
 
-            if value < 5:
-                os.remove(pathname)
+        out.release()
 
     def getPath(self):
         return self.path
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import imageProcessor
 
 
 def main():
-    video_filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Input\\WIN_20210628_13_21_13_Pro.mp4"
+    video_filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Input\\WIN_20210628_14_49_54_Pro.mp4"
     processor = imageProcessor.ImageProcessor("C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\")
     processor.split(video_filename)
-    processor.removeBlur()
+    processor.recreateVideo()
 
 
 if __name__ == '__main__':
