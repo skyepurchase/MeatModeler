d2f3db467ddffa4fecf9313d26efa5daa3d43702
Skye <aidantpurchase@gmail.com>
2021-07-01 12:08:57 +0100

    standard library names

--- imageRectifier.py
+++ imageRectifier.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2 as cv
+import cv2
 
 
 def getDescriptors(img):
@@ -23,7 +23,7 @@ class ImageRectifier:
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
-        flann = cv.FlannBasedMatcher(index_params, search_params)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
         matches = flann.knnMatch(des1, des2, k=2)
         pts1 = []
         pts2 = []
@@ -36,7 +36,7 @@ class ImageRectifier:
 
         pts1 = np.int32(pts1)
         pts2 = np.int32(pts2)
-        F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_LMEDS)
+        F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
 
         if F is not None:
             # We select only inlier points
@@ -46,30 +46,30 @@ class ImageRectifier:
         return F, pts1, pts2
 
     def rectify(self, src1, src2):
-        img1 = cv.imread(src1)
-        img2 = cv.imread(src2)
+        img1 = cv2.imread(src1)
+        img2 = cv2.imread(src2)
         F, pts1, pts2 = self.calculateF(img1, img2)
 
         if F is None:
             return None
 
         try:
-            success, H1, H2 = cv.stereoRectifyUncalibrated(pts1, pts2, F, (img1.shape[1], img1.shape[0]))
+            success, H1, H2 = cv2.stereoRectifyUncalibrated(pts1, pts2, F, (img1.shape[1], img1.shape[0]))
             H1_inv = np.linalg.inv(H1)
 
-            img1_wrap = cv.warpPerspective(img1, H1, (img1.shape[1], img1.shape[0]))
-            img2_wrap = cv.warpPerspective(img2, H2, (img2.shape[1], img2.shape[0]))
-            img2_mosaic = cv.warpPerspective(img2_wrap, H1_inv, (img2_wrap.shape[1], img2_wrap.shape[0]))
+            img1_wrap = cv2.warpPerspective(img1, H1, (img1.shape[1], img1.shape[0]))
+            img2_wrap = cv2.warpPerspective(img2, H2, (img2.shape[1], img2.shape[0]))
+            img2_mosaic = cv2.warpPerspective(img2_wrap, H1_inv, (img2_wrap.shape[1], img2_wrap.shape[0]))
 
             name1 = src1.split("\\")[-1].split(".")[0]
             name2 = src2.split("\\")[-1].split(".")[0]
             filenameL = self.path + name1 + "-" + name2 + "-L.jpg"
             filenameR = self.path + name1 + "-" + name2 + "-R.jpg"
-            cv.imwrite(filenameL, img1_wrap)
-            cv.imwrite(filenameR, img2_wrap)
+            cv2.imwrite(filenameL, img1_wrap)
+            cv2.imwrite(filenameR, img2_wrap)
 
             return img1_wrap, img2_wrap
-        except cv.error as e:
+        except cv2.error as e:
             # TODO: Deal with this mismatch error
             print(e)
             return None
