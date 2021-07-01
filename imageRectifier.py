6523c1d8f2a083fdcfae38496aa12c63b45a749e
Skye <aidantpurchase@gmail.com>
2021-07-01 10:15:30 +0100

    Refactoring imageRectifier.py and fully pairwise mosaics

--- imageRectifier.py
+++ imageRectifier.py
@@ -2,76 +2,95 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img1 = cv.imread('C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\frame8.jpg', 0)  # queryimage # left image
-img2 = cv.imread('C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\frame9.jpg', 0)  # trainimage # right image
-sift = cv.SIFT_create()
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
+def drawlines(img1, img2, lines, pts_in1, pts_in2):
+    r, c = img1.shape
+    img1 = cv.cvtColor(img1, cv.COLOR_GRAY2BGR)
+    img2 = cv.cvtColor(img2, cv.COLOR_GRAY2BGR)
 
-# FLANN parameters
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=50)
-flann = cv.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(des1, des2, k=2)
-pts1 = []
-pts2 = []
-
-# ratio test as per Lowe's paper
-for i, (m, n) in enumerate(matches):
-    if m.distance < 0.8 * n.distance:
-        pts2.append(kp2[m.trainIdx].pt)
-        pts1.append(kp1[m.queryIdx].pt)
-
-pts1 = np.int32(pts1)
-pts2 = np.int32(pts2)
-F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_LMEDS)
-
-# We select only inlier points
-pts1 = pts1[mask.ravel() == 1]
-pts2 = pts2[mask.ravel() == 1]
-
-
-def drawlines(img_in1, img_in2, lines, pts_in1, pts_in2):
-    """ img1 - image on which we draw the epilines for the points in img2
-        lines - corresponding epilines """
-    r, c = img_in1.shape
-    img_in1 = cv.cvtColor(img_in1, cv.COLOR_GRAY2BGR)
-    img_in2 = cv.cvtColor(img_in2, cv.COLOR_GRAY2BGR)
     for r, pt1, pt2 in zip(lines, pts_in1, pts_in2):
         color = tuple(np.random.randint(0, 255, 3).tolist())
         x0, y0 = map(int, [0, -r[2] / r[1]])
         x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
-        cv.line(img_in1, (x0, y0), (x1, y1), color, 1)
-        cv.circle(img_in1, tuple(pt1), 5, color, -1)
-        cv.circle(img_in2, tuple(pt2), 5, color, -1)
-    return img_in1, img_in2
+
+        cv.line(img1, (x0, y0), (x1, y1), color, 1)
+        cv.circle(img1, tuple(pt1), 5, color, -1)
+        cv.circle(img2, tuple(pt2), 5, color, -1)
+
+    return img1, img2
 
 
-# Find epilines corresponding to points in right image (second image) and
-# drawing its lines on left image
-lines1 = cv.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F)
-lines1 = lines1.reshape(-1, 3)
-img5, img6 = drawlines(img1, img2, lines1, pts1, pts2)
+class ImageRectifier:
+    def __init__(self, path):
+        self.path = path
 
-# Find epilines corresponding to points in left image (first image) and
-# drawing its lines on right image
-lines2 = cv.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F)
-lines2 = lines2.reshape(-1, 3)
-img3, img4 = drawlines(img2, img1, lines2, pts2, pts1)
+    def calculateF(self, img1, img2):
+        sift = cv.SIFT_create()
 
-plt.subplot(221), plt.imshow(img5)
-plt.subplot(222), plt.imshow(img3)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
 
-success, H1, H2 = cv.stereoRectifyUncalibrated(pts1, pts2, F, (img1.shape[1], img1.shape[0]))
-H1_inv = np.linalg.inv(H1)
-img1_wrap = cv.warpPerspective(img5, H1, (img5.shape[1], img5.shape[0]))
-img2_wrap = cv.warpPerspective(img3, H2, (img3.shape[1], img3.shape[0]))
-img2_wrap = cv.warpPerspective(img2_wrap, H1_inv, (img2_wrap.shape[1], img2_wrap.shape[0]))
-test = cv.addWeighted(img5, 0.5, img2_wrap, 0.5, 0)
+        # FLANN parameters
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1, des2, k=2)
+        pts1 = []
+        pts2 = []
 
-plt.subplot(223), plt.imshow(test)
-plt.subplot(224), plt.imshow(img2_wrap)
-plt.show()
+        # ratio test as per Lowe's paper
+        for i, (m, n) in enumerate(matches):
+            if m.distance < 0.8 * n.distance:
+                pts2.append(kp2[m.trainIdx].pt)
+                pts1.append(kp1[m.queryIdx].pt)
+
+        pts1 = np.int32(pts1)
+        pts2 = np.int32(pts2)
+        F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_LMEDS)
+
+        # We select only inlier points
+        pts1 = pts1[mask.ravel() == 1]
+        pts2 = pts2[mask.ravel() == 1]
+
+        return F, pts1, pts2
+
+    def drawEpilines(self, img1, img2, data=None):
+        if data is None or len(data) != 3:
+            F, pts1, pts2 = self.calculateF(img1, img2)
+        else:
+            F, pts1, pts2 = data
+
+        lines1 = cv.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F)
+        lines1 = lines1.reshape(-1, 3)
+        img1_out, img2_dots = drawlines(img1, img2, lines1, pts1, pts2)
+
+        lines2 = cv.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F)
+        lines2 = lines2.reshape(-1, 3)
+        img2_out, img1_dots = drawlines(img2, img1, lines2, pts2, pts1)
+
+        return img1_out, img2_out
+
+    def rectify(self, src1, src2, display_lines=False):
+        img1 = cv.imread(src1)
+        img2 = cv.imread(src2)
+        F, pts1, pts2 = self.calculateF(img1, img2)
+
+        success, H1, H2 = cv.stereoRectifyUncalibrated(pts1, pts2, F, (img1.shape[1], img2.shape[0]))
+        H1_inv = np.linalg.inv(H1)
+
+        if display_lines:
+            img1, img2 = self.drawEpilines(img1, img2, (F, pts1, pts2))
+
+        img1_wrap = cv.warpPerspective(img1, H1, (img1.shape[1], img1.shape[0]))
+        img2_wrap = cv.warpPerspective(img2, H2, (img2.shape[1], img2.shape[0]))
+        img2_mosaic = cv.warpPerspective(img2_wrap, H1_inv, (img2_wrap.shape[1], img2_wrap.shape[0]))
+        mosaic = cv.addWeighted(img1, 0.5, img2_mosaic, 0.5, 0)
+
+        name1 = src1.split("\\")[-1].split(".")[0]
+        name2 = src2.split("\\")[-1].split(".")[0]
+        filename = self.path + name1 + "-" + name2 + "-mosaic.jpg"
+        cv.imwrite(filename, mosaic)
+
+        return img1_wrap, img2_wrap, mosaic
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import imageProcessor
-import depthDetection
+import imageRectifier
+import os
 
+samples = 60
+source = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Input\\"
+raw_output = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\Raw\\"
+rectified_output = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\Rectified\\"
 
 def main():
-    video_filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Input\\WhatsApp Video 2021-06-28 at 16.24.37.mp4"
-    processor = imageProcessor.ImageProcessor("C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\",
-                                              60,
-                                              blur_threshold=5,
-                                              clahe_threshold=3.5)
+    video_filename = source + "WhatsApp Video 2021-06-28 at 16.24.37.mp4"
+    processor = imageProcessor.ImageProcessor(raw_output, 60)
     processor.split(video_filename)
-    # depthDectector = depthDetection.DepthDetector("C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\", 400)
-    # depthDectector.run()
+    rectifier = imageRectifier.ImageRectifier(rectified_output)
+
+    prev_src = ""
+    for i in range(samples):
+        if i > 0:
+            rectifier.rectify(prev_src, raw_output + "Frame" + str(i) + ".jpg")
+            print("Rectified", i)
+
+        prev_src = raw_output + "Frame" + str(i) + ".jpg"
 
 
 if __name__ == '__main__':
