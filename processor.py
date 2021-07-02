a0b06562d53ec5a16b0086f1875e8f6e93685163
Skye <aidantpurchase@gmail.com>
2021-07-02 14:49:53 +0100

    Track a user defined ROI

--- processor.py
+++ processor.py
@@ -13,7 +13,26 @@ class Processor:
         :return: A 3D mesh
         """
         cap = cv2.VideoCapture(video)
+        tracker = cv2.TrackerMIL_create()
 
         success, frame = cap.read()
+
+        if success:
+            bbox = cv2.selectROI(frame, False)
+            tracker.init(frame, bbox)
+
+        success, frame = cap.read()
+
         while success:
+            ok, bbox = tracker.update(frame)
+
+            if ok:
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+
+            cv2.imshow("Tracking", frame)
+            cv2.waitKey()
+
             success, frame = cap.read()
+
