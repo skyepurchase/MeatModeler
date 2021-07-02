7f45732f8c5e4a27f31778f85d1a3562d85fd5a6
Skye <aidantpurchase@gmail.com>
2021-07-02 16:38:15 +0100

    Tracking based on keyframes

--- processor.py
+++ processor.py
@@ -2,16 +2,27 @@ import cv2
 import numpy as np
 
 
+def kp2pt(kp):
+    print(kp.pt)
+    return kp.pt
+
+
+kp2pt_v = np.vectorize(kp2pt)
+
+
 class Processor:
     def __init__(self):
         self.feature_params = dict(maxCorners=100,
                                    qualityLevel=0.3,
                                    minDistance=7,
                                    blockSize=7)
-        self.lk_params = dict(winSize=(15,15),
+        self.lk_params = dict(winSize=(15, 15),
                               maxLevel=3,
                               criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
         self.color = np.random.randint(0, 255, (100, 3))
+        self.fast = cv2.FastFeatureDetector_create()
+        self.fast.setNonmaxSuppression(0)
+        self.orb = cv2.ORB_create()
 
     def process(self, video):
         """
@@ -21,37 +32,43 @@ class Processor:
         """
         cap = cv2.VideoCapture(video)
 
-        success, old_frame = cap.read()
-        old_grey = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-        p0 = cv2.goodFeaturesToTrack(old_grey, mask=None, **self.feature_params)
+        success, start_frame = cap.read()
+        orig_grey = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
+        orig_p0 = cv2.goodFeaturesToTrack(orig_grey, mask=None, **self.feature_params)
+        count = 0
 
-        mask = np.zeros_like(old_frame)
+        # mask = np.zeros_like(start_frame)
 
         success, frame = cap.read()
-        frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         while success:
-            p1, st, err = cv2.calcOpticalFlowPyrLK(old_grey, frame_grey, p0, None, **self.lk_params)
+            frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            p1, st, err = cv2.calcOpticalFlowPyrLK(orig_grey, frame_grey, orig_p0, None, **self.lk_params)
 
             if p1 is not None:
-                good_new = p1[st==1]
-                good_old = p0[st==1]
+                # good_new = p1[st == 1]
+                good_old = orig_p0[st == 1]
 
-            for i, (new, old) in enumerate(zip(good_new, good_old)):
-                a, b = new.ravel()
-                c, d = old.ravel()
-                mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), self.color[i].tolist(), 2)
-                frame = cv2.circle(frame, (int(a), int(b)), 5, self.color[i].tolist(), -1)
+            # for i, (new, old) in enumerate(zip(good_new, good_old)):
+            #     a, b = new.ravel()
+            #     c, d = old.ravel()
+            #     frame = cv2.line(frame, (int(a), int(b)), (int(c), int(d)), self.color[i].tolist(), 2)
+            #     frame = cv2.circle(frame, (int(a), int(b)), 5, self.color[i].tolist(), -1)
+            #
+            # img = cv2.add(frame, mask)
+            #
+            # cv2.imshow("Tracking", img)
+            # key = cv2.waitKey() & 0xff
+            # if key == 27:
+            #     break
 
-            img = cv2.add(frame, mask)
-
-            cv2.imshow("Tracking", img)
-            key = cv2.waitKey() & 0xff
-            if key == 27:
-                break
-
-            old_grey = frame_grey.copy()
-            p0 = good_new.reshape(-1, 1, 2)
+            if err is None or np.average(err) > 30:
+                filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\Raw\\Image" + str(count) + ".jpg"
+                cv2.imwrite(filename, orig_grey)
+                orig_grey = frame_grey
+                orig_p0 = cv2.goodFeaturesToTrack(orig_grey, mask=None, **self.feature_params)
+                count += 1
+            else:
+                orig_p0 = good_old.reshape(-1, 1, 2)
 
             success, frame = cap.read()
-            frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
