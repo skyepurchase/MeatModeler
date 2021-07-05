190d63a8fc0009a2324403ce9d3c909324fb8595
Skye <aidantpurchase@gmail.com>
2021-07-05 09:11:34 +0100

    Start of calculating new points

--- processor.py
+++ processor.py
@@ -28,7 +28,7 @@ def increaseContrast(frame):
 
 
 class Processor:
-    def __init__(self, display=False):
+    def __init__(self):
         self.feature_params = dict(maxCorners=100,
                                    qualityLevel=0.3,
                                    minDistance=7,
@@ -36,15 +36,22 @@ class Processor:
         self.lk_params = dict(winSize=(15, 15),
                               maxLevel=3,
                               criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        self.flann_params = dict(algorithm=6,
+                                 table_number=6,
+                                 key_size=12,
+                                 multi_probe_level=2)
 
         self.current_keyframe_grey = None
         self.current_keyframe_points = None
 
+        self.current_orb_points = None
+        self.current_orb_descriptors = None
+        self.point_vector = []
+        self.orb = cv2.ORB_create(nfeatures=2000)
+
         self.color = np.random.randint(0, 255, (100, 3))
-
-        self.fast = cv2.FastFeatureDetector_create()
-
-        self.display = display
+        self.display = False
+        self.count = 0
 
     def process(self, video, display=False):
         """
@@ -61,6 +68,7 @@ class Processor:
 
         # Extract features from the start frame
         _, start_frame = cap.read()
+        self.current_orb_points, self.current_orb_descriptors = self.orb.detectAndCompute(start_frame, None)
         self.current_keyframe_grey = cv2.cvtColor(increaseContrast(start_frame), cv2.COLOR_BGR2GRAY)
         self.current_keyframe_points = cv2.goodFeaturesToTrack(self.current_keyframe_grey,
                                                                mask=None,
@@ -70,9 +78,15 @@ class Processor:
         success, frame = cap.read()
         while success:
             if self.isKeyframe(frame):
-                pass
+                self.point_vector.append(self.calculateMatchedPoints(frame))
+                self.current_keyframe_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                self.current_keyframe_points = cv2.goodFeaturesToTrack(self.current_keyframe_grey,
+                                                                       mask=None,
+                                                                       **self.feature_params)
 
             success, frame = cap.read()
+
+        print(self.point_vector)
 
     def isKeyframe(self, frame):
         """
@@ -109,15 +123,32 @@ class Processor:
 
         # Current frame has deviated enough to be considered a key frame
         if err is not None and np.average(err) > 30:
-            # ReCalculate features and change to new keyframe
-            self.current_keyframe_grey = frame_grey
-            self.current_keyframe_points = cv2.goodFeaturesToTrack(self.current_keyframe_grey,
-                                                                   mask=None,
-                                                                   **self.feature_params)
-
             return True
         else:
-            # Only want visible points
+            # Keep only visible points
             self.current_keyframe_points = good_old.reshape(-1, 1, 2)
 
             return False
+
+    def calculateMatchedPoints(self, keyframe):
+        """
+        Finds which features in two keyframes match
+        :param keyframe: The keyframe to compare to the previous keyframe
+        :return: List of points
+        """
+        # TODO: convert to find new points efficiently
+        # Get new points and descriptors
+        new_points, new_descriptors = self.orb.detectAndCompute(keyframe, None)
+
+        # FLANN based approach to find matches
+        flann = cv2.FlannBasedMatcher(self.flann_params, {})
+        matches = flann.knnMatch(self.current_orb_descriptors, new_descriptors, k=2)
+
+        # Find which points can be considered matches
+        good_matches = [match[0] for match in matches if
+                        len(match) == 2 and match[0].distance < 0.7 * match[1].distance]
+
+        # Update the keyframe points
+        self.current_orb_points, self.current_orb_descriptors = new_points, new_descriptors
+
+        return good_matches
