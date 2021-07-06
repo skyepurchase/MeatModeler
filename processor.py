330e4f391ca809d97fca952ad6e5f55c540e7ec0
Skye <aidantpurchase@gmail.com>
2021-07-06 16:33:43 +0100

    Made calibrate, keyframe tracking, and feature tracking static

--- processor.py
+++ processor.py
@@ -3,6 +3,8 @@ import numpy as np
 from track import Track
 
 
+# TODO: make all methods stateless
+
 def increaseContrast(frame):
     """
     Increases the contrast of the grey scale images by applying CLAHE to the luminance
@@ -20,6 +22,135 @@ def increaseContrast(frame):
     return cv2.cvtColor(lab_out, cv2.COLOR_Lab2BGR)
 
 
+def calibrate(images):
+    # Prepare chessboard 3D points
+    objp = np.zeros((7 * 7, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+
+    # Arrays to store object and image points from all images
+    obj_points = []
+    img_points = []
+
+    for filename in images:
+        img = cv2.imread(filename)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chessboard corners
+        success, corners = cv2.findChessboardCorners(gray, (7, 7), None)
+
+        # If found, add object points, image points
+        if success:
+            obj_points.append(objp)
+            img_points.append(corners)
+
+    img = cv2.imread(images[0])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    success, matrix, distortion, _, _ = cv2.calibrateCamera(obj_points,
+                                                            img_points,
+                                                            gray.shape[::-1],
+                                                            None,
+                                                            None)
+    if success:
+        return matrix, distortion
+
+    return None
+
+
+def keyframeTracking(frame_grey, prev_frame_grey, prev_frame_points, accumulated_error, lk_params, feature_params,
+                     threshold=0.3, display=False, color=None, mask=None):
+    """
+    Determines whether a given frame is a keyframe for further analysis
+    :param frame_grey: The frame to be analysed in greyscale
+    :param prev_frame_grey: The previous frame in greyscale
+    :param prev_frame_points: The previous frame features to track
+    :param accumulated_error: The current error distance from keyframe points
+    :param lk_params: Lucas-Kanade parameters
+    :param feature_params: GoodFeaturesToTrack parameters
+    :param threshold: Proportion of the frame width considered significant
+    :param display: Whether to display the process
+    :param color: The colour of the display points
+    :param mask: The mask for displaying
+    :return: A boolean value on whether the frame was a keyframe,
+            The new previous grey frame,
+            The new previous frame points,
+            The new accumulated error
+    """
+    # Compare the last key frame to current key frame
+    p, st, err = cv2.calcOpticalFlowPyrLK(prev_frame_grey,
+                                          frame_grey,
+                                          prev_frame_points,
+                                          None,
+                                          **lk_params)
+
+    # Keep only matching points
+    if p is not None:
+        good_new = p[st == 1]
+        good_prev = prev_frame_points[st == 1]
+
+        # Will be removed later
+        if display:
+            for i, (new, old) in enumerate(zip(good_new, good_prev)):
+                a, b = new.ravel()
+                c, d = old.ravel()
+                mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+                frame_grey = cv2.circle(frame_grey, (int(a), int(b)), 5, color[i].tolist(), -1)
+
+            img = cv2.add(frame_grey, mask)
+            cv2.imshow("Tracking", img)
+            cv2.waitKey()
+
+        # Update previous data
+        prev_frame_grey = frame_grey
+        prev_frame_points = good_new.reshape(-1, 1, 2)
+
+        # If possible increase the accumulative error between frames
+        if err is not None:
+            accumulated_error += np.average(err)
+
+        # Current frame has deviated enough to be considered a key frame
+        if accumulated_error > threshold * frame_grey.shape[1]:
+            accumulated_error = 0
+
+            # Recalculate points for new keyframe
+            prev_frame_points = cv2.goodFeaturesToTrack(prev_frame_grey,
+                                                        mask=None,
+                                                        **feature_params)
+
+            return True, prev_frame_grey, prev_frame_points, accumulated_error
+        else:
+            return False, prev_frame_grey, prev_frame_points, accumulated_error
+
+
+def featureTracking(new_keyframe, prev_orb_points, prev_orb_descriptors, orb, flann_params):
+    """
+    Finds which features in two keyframes match
+    :param new_keyframe: The keyframe to compare to the previous keyframe
+    :param prev_orb_points: The previous keyframe feature points
+    :param prev_orb_descriptors: The previous keyframe feature descriptors
+    :param orb: An ORB feature detection object
+    :param flann_params: Parameters to tune FLANN matcher
+    :return: List of matched Keypoints,
+            The new previous keyframe feature points
+            The new previous keyframe feature descriptors
+    """
+    # Get new points and descriptors
+    new_points, new_descriptors = orb.detectAndCompute(new_keyframe, None)
+
+    # FLANN based approach to find matches
+    flann = cv2.FlannBasedMatcher(flann_params, {})
+    matches = flann.knnMatch(prev_orb_descriptors, new_descriptors, k=2)
+
+    # Find which points can be considered new
+    # TODO: vectorise following calculations
+    good_matches = [match[0] for match in matches if
+                    len(match) == 2 and match[0].distance < 0.8 * match[1].distance]
+    point_matches = np.array(
+        [[prev_orb_points[m.queryIdx].pt, new_points[m.trainIdx].pt] for m in good_matches])
+
+    return point_matches, new_points, new_descriptors
+
+
 class Processor:
     def __init__(self, images):
         self.feature_params = dict(maxCorners=100,
@@ -34,16 +165,10 @@ class Processor:
                                  key_size=12,
                                  multi_probe_level=2)
 
-        self.intrinsic, self.distortion = self.calibrate(images)
+        self.intrinsic, self.distortion = calibrate(images)
 
-        self.prev_frame_grey = None
-        self.prev_frame_points = None
         self.prev_keyframe_grey = None
-        self.acc_error = 0
 
-        self.current_orb_points = None
-        self.current_orb_descriptors = None
-        self.point_vector = []
         self.orb = cv2.ORB_create(nfeatures=2000)
 
         self.tracks = []
@@ -55,40 +180,7 @@ class Processor:
         self.display = False
         self.mask = None
         self.count = 1
-
-    def calibrate(self, images):
-        # Prepare chessboard 3D points
-        objp = np.zeros((7 * 7, 3), np.float32)
-        objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
-
-        # Arrays to store object and image points from all images
-        obj_points = []
-        img_points = []
-
-        for filename in images:
-            img = cv2.imread(filename)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-            # Find the chessboard corners
-            success, corners = cv2.findChessboardCorners(gray, (7, 7), None)
-
-            # If found, add object points, image points
-            if success:
-                obj_points.append(objp)
-                img_points.append(corners)
-
-        img = cv2.imread(images[0])
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        success, matrix, distortion, _, _ = cv2.calibrateCamera(obj_points,
-                                                                img_points,
-                                                                gray.shape[::-1],
-                                                                None,
-                                                                None)
-        if success:
-            return matrix, distortion
-
-        return None
+        self.num_features = 0
 
     def process(self, video, display=False):
         """
@@ -102,17 +194,23 @@ class Processor:
 
         cap = cv2.VideoCapture(video)
 
-        # Extract features from the start frame
+        # Retrieve first frame
         _, start_frame = cap.read()
-        self.prev_frame_grey = cv2.cvtColor(increaseContrast(start_frame), cv2.COLOR_BGR2GRAY)
-        self.prev_keyframe_grey = self.prev_frame_grey
-        self.prev_frame_points = cv2.goodFeaturesToTrack(self.prev_frame_grey,
-                                                         mask=None,
-                                                         **self.feature_params)
-        self.current_orb_points, self.current_orb_descriptors = self.orb.detectAndCompute(self.prev_frame_grey, None)
+        filename = "C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\Raw\\Image0.jpg"
+        cv2.imwrite(filename, start_frame)
+
+        # Initialise keyframe tracking
+        prev_frame_grey = cv2.cvtColor(increaseContrast(start_frame), cv2.COLOR_BGR2GRAY)
+        self.prev_keyframe_grey = prev_frame_grey
+        prev_frame_points = cv2.goodFeaturesToTrack(prev_frame_grey,
+                                                    mask=None,
+                                                    **self.feature_params)
+        accumulative_error = 0
+
+        # Initiliase feature tracking
+        prev_orb_points, prev_orb_descriptors = self.orb.detectAndCompute(prev_frame_grey, None)
 
         # Will be removed
-        cv2.imwrite("C:\\Users\\aidan\\Documents\\BrevilleInternship\\Output\\Raw\\Image0.jpg", start_frame)
         self.mask = np.zeros_like(start_frame)
 
         # Processing loop
@@ -120,10 +218,22 @@ class Processor:
 
         while success:
             frame_grey = cv2.cvtColor(increaseContrast(frame), cv2.COLOR_BGR2GRAY)
+            success, prev_frame_grey, prev_frame_points, accumulative_error = keyframeTracking(frame_grey,
+                                                                                               prev_frame_grey,
+                                                                                               prev_frame_points,
+                                                                                               accumulative_error,
+                                                                                               self.lk_params,
+                                                                                               self.feature_params)
 
-            if self.isKeyframe(frame_grey):
+            if success:
+                self.prev_keyframe_grey = frame_grey
+
                 # Calculate matches
-                matches = self.calculateMatchedPoints(frame_grey)
+                matches, prev_orb_points, prev_orb_descriptors = featureTracking(frame_grey,
+                                                                                 prev_orb_points,
+                                                                                 prev_orb_descriptors,
+                                                                                 self.orb,
+                                                                                 self.flann_params)
 
                 # Pose estimation
                 R, t = self.findRotationAndTranslation(matches)
@@ -131,9 +241,11 @@ class Processor:
                 # Update tracks
                 self.manageTracks(frame_grey, matches)
 
+                # Will go onto Triangulation/Bundling
                 for track in self.tracks:
-                    if track.wasUpdated():
+                    if not track.wasUpdated():
                         self.tracks.remove(track)
+                        self.num_features += 1
 
                 # Will be removed later
                 self.mask = np.zeros_like(frame)
@@ -143,83 +255,6 @@ class Processor:
                 self.count += 1
 
             success, frame = cap.read()
-
-    def isKeyframe(self, frame):
-        """
-        Determines whether a given frame is a keyframe for further analysis
-        :param frame: The frame to be analysed
-        :return: A boolean value on whether the frame was a keyframe
-        """
-        # Compare the last key frame to current key frame
-        p, st, err = cv2.calcOpticalFlowPyrLK(self.prev_frame_grey,
-                                              frame,
-                                              self.prev_frame_points,
-                                              None,
-                                              **self.lk_params)
-
-        # Keep only matching points
-        if p is not None:
-            good_new = p[st == 1]
-            good_prev = self.prev_frame_points[st == 1]
-
-            # Will be removed later
-            if self.display:
-                for i, (new, old) in enumerate(zip(good_new, good_prev)):
-                    a, b = new.ravel()
-                    c, d = old.ravel()
-                    self.mask = cv2.line(self.mask, (int(a), int(b)), (int(c), int(d)), self.color[i].tolist(), 2)
-                    frame = cv2.circle(frame, (int(a), int(b)), 5, self.color[i].tolist(), -1)
-
-                img = cv2.add(frame, self.mask)
-                cv2.imshow("Tracking", img)
-                key = cv2.waitKey() & 0xff
-                if key == 27:
-                    self.display = False
-
-            # Update previous data
-            self.prev_frame_grey = frame
-            self.prev_frame_points = good_new.reshape(-1, 1, 2)
-
-            # If possible increase the accumulative error between frames
-            if err is not None:
-                self.acc_error += np.average(err)
-
-            # Current frame has deviated enough to be considered a key frame
-            if self.acc_error > 0.3 * frame.shape[1]:
-                self.acc_error = 0
-
-                # Recalculate points for new keyframe
-                self.prev_frame_points = cv2.goodFeaturesToTrack(self.prev_frame_grey,
-                                                                 mask=None,
-                                                                 **self.feature_params)
-                return True
-            else:
-                return False
-
-    def calculateMatchedPoints(self, keyframe):
-        """
-        Finds which features in two keyframes match
-        :param keyframe: The keyframe to compare to the previous keyframe
-        :return: List of keypoints
-        """
-        # Get new points and descriptors
-        new_points, new_descriptors = self.orb.detectAndCompute(keyframe, None)
-
-        # FLANN based approach to find matches
-        flann = cv2.FlannBasedMatcher(self.flann_params, {})
-        matches = flann.knnMatch(self.current_orb_descriptors, new_descriptors, k=2)
-
-        # Find which points can be considered new
-        # TODO: vectorise following calculations
-        good_matches = [match[0] for match in matches if
-                        len(match) == 2 and match[0].distance < 0.8 * match[1].distance]
-        point_matches = np.array(
-            [[self.current_orb_points[m.queryIdx].pt, new_points[m.trainIdx].pt] for m in good_matches])
-
-        # Update the keyframe points
-        self.current_orb_points, self.current_orb_descriptors = new_points, new_descriptors
-
-        return point_matches
 
     def manageTracks(self, keyframe, matches):
         """
