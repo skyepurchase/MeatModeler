e1103df86b48f8f4c2b8715d1a7f30daaf4c0430
Skye <aidantpurchase@gmail.com>
2021-07-07 13:47:21 +0100

    Update track.py for new approach

--- track.py
+++ track.py
@@ -1,14 +1,15 @@
 class Track:
-    def __init__(self, prev_frame, point, frame, correspondent):
-        self.first_frame = prev_frame
-        self.last_frame = frame
+    def __init__(self, prev_frame_ID, point, frame_ID, correspondent, physical_point):
+        self.first_frame_ID = prev_frame_ID
+        self.last_frame_ID = frame_ID
         self.virtual_point_vector = [point, correspondent]
-        self.physical_point = None
+        self.physical_point_vector = [physical_point]
         self.updated = True
 
-    def update(self, frame, correspondent):
-        self.last_frame = frame
+    def update(self, frame_ID, correspondent, point):
+        self.last_frame_ID = frame_ID
         self.virtual_point_vector.append(correspondent)
+        self.physical_point_vector.append(point)
         self.updated = True
 
     def reset(self):
@@ -18,10 +19,7 @@ class Track:
         return self.virtual_point_vector[-1]
 
     def getTriangulationData(self):
-        return self.first_frame, self.last_frame, self.virtual_point_vector[0], self.virtual_point_vector[-1]
-
-    def setPhysicalPoint(self, point):
-        self.physical_point = point
+        return self.first_frame_ID, self.last_frame_ID, self.virtual_point_vector, self.physical_point_vector
 
     def wasUpdated(self):
         return self.updated
