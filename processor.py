9351167684823739167ec81c3431adc519854764
Skye <aidantpurchase@gmail.com>
2021-07-19 10:46:34 +0100

    Temporarily removing bundling

--- processor.py
+++ processor.py
@@ -608,20 +608,22 @@ def process(video, path, intrinsic_matrix, distortion_coefficients, lk_params, f
     print(len(points), "points found.")
     print(toc - tic, "seconds.\n")
 
-    print("adjusting points...")
-    tic = time.time()
-    points = bundleAdjuster.bundleAdjustment(np.array(transforms),
-                                             intrinsic_matrix,
-                                             points,
-                                             np.array(points_2d),
-                                             np.array(frame_indices),
-                                             np.array(point_indices))
-    toc = time.time()
-    print("adjustment complete.")
-    print(toc - tic, "seconds.\n")
+    # print("adjusting points...")
+    # tic = time.time()
+    #
+    # points, translations = bundleAdjuster.bundleAdjustment(np.array(transforms),
+    #                                                        intrinsic_matrix,
+    #                                                        points,
+    #                                                        np.array(points_2d),
+    #                                                        np.array(frame_indices),
+    #                                                        np.array(point_indices))
+    #
+    # toc = time.time()
+    # print("adjustment complete.")
+    # print(toc - tic, "seconds.\n")
 
-    # Convert points to world coordinates
-    points = np.einsum("ij,...j", intrinsic_matrix, points)
+    print("Saving point cloud...")
+    tic = time.time()
 
     filename = path + "Cloud.ply"
     cloud = PyntCloud(pd.DataFrame(
